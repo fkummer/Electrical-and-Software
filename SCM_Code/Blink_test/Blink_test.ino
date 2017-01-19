d19d20ed471643ad4941682bb503c0c2e9ba56f9
Frederick Kummer <fck22@cornell.edu>
2017-01-19 14:22:03 -0500

    Some schematic updates, spi test is working

--- SCM_Code/Blink_test/Blink_test.ino
+++ SCM_Code/Blink_test/Blink_test.ino
@@ -2,8 +2,6 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
@@ -12,17 +10,14 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(3, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(4, HIGH);   // turn the LED on (HIGH is the voltage level)
+
   digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(6, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(7, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(3, LOW);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(4, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(250);                       // wait for a second
   digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
   digitalWrite(6, LOW);    // turn the LED off by making the voltage LOW
   digitalWrite(7, LOW);    // turn the LED off by making the voltage LOW
-  Serial.println("yello");
-  delay(1000);                       // wait for a second
+  //Serial.println("yello");
+  delay(250);                       // wait for a second
 }
